5ebff062072b701765ce07080ac1f16733d5ca55
Richard Lincoln <r.w.lincoln@gmail.com>
2015-05-14 13:38:03 +0200

    Converting example to use web workers.

--- example/example.dart
+++ example/example.dart
@@ -1,47 +1,66 @@
 
 import 'dart:html';
+import 'dart:async';
 import 'package:graphviz/graphviz.dart';
 import 'package:graphviz/graphs.dart';
 
 Node inspect(String s) => new Element.pre()..appendText(s);
 
-Node example(String dotData, Render format, [Layout engine=Layout.DOT]) {
+Future<Node> example(Graphviz gv, String dotData, Render format, [Layout engine=Layout.DOT]) {
   var result;
-  try {
-    result = graphviz(dotData, render: format, layout: engine);
+  return gv.layout(dotData, render: format, layout: engine).then((result) {
     if (format == Render.SVG) {
       final doc = new DomParser().parseFromString(result, 'text/xml');
       return document.importNode(doc.documentElement, true);
     } else {
       return inspect(result);
     }
-  } catch(e) {
+  }, onError: (e) {
     return inspect(e.toString());
-  }
+  });
 }
 
 main() {
-  document.body.appendHtml("<h1>Cluster (svg output)</h1>");
-  document.body.append(example(cluster, Render.SVG));
+  var gv1 = new Graphviz();
+  var gv2 = new Graphviz();
 
-  document.body.appendHtml("<h1>Crazy (svg output)</h1>");
-  document.body.append(example(crazy, Render.SVG));
+  example(gv1, cluster, Render.SVG).then((node) {
+    document.body.appendHtml("<h1>Cluster (svg output)</h1>");
+    document.body.append(node);
+  });
 
-  document.body.appendHtml("<h1>Data Structures (svg output)</h1>");
-  document.body.append(example(data, Render.SVG));
+  example(gv2, crazy, Render.SVG).then((node) {
+    document.body.appendHtml("<h1>Crazy (svg output)</h1>");
+    document.body.append(node);
+  });
 
-  document.body.appendHtml("<h1>Tables (svg output)</h1>");
-  document.body.append(example(tables, Render.SVG));
+  example(gv1, data, Render.SVG).then((node) {
+    document.body.appendHtml("<h1>Data Structures (svg output)</h1>");
+    document.body.append(node);
+  });
 
-  document.body.appendHtml("<h1>Gradients (svg output)</h1>");
-  document.body.append(example(gradients, Render.SVG));
+  example(gv2, tables, Render.SVG).then((node) {
+    document.body.appendHtml("<h1>Tables (svg output)</h1>");
+    document.body.append(node);
+  });
 
-  document.body.appendHtml("<h1>Process States (svg output, neato engine)</h1>");
-  document.body.append(example(process, Render.SVG, Layout.NEATO));
+  example(gv1, gradients, Render.SVG).then((node) {
+    document.body.appendHtml("<h1>Gradients (svg output)</h1>");
+    document.body.append(node);
+  });
 
-  document.body.appendHtml("<h1>Cluster (plain output)</h1>");
-  document.body.append(example(cluster, Render.PLAIN));
+  example(gv2, process, Render.SVG, Layout.NEATO).then((node) {
+    document.body.appendHtml("<h1>Process States (svg output, neato engine)</h1>");
+    document.body.append(node);
+  });
 
-  document.body.appendHtml("<h1>Cluster (xdot output)</h1>");
-  document.body.append(example(cluster, Render.XDOT));
+  example(gv1, cluster, Render.PLAIN).then((node) {
+    document.body.appendHtml("<h1>Cluster (plain output)</h1>");
+    document.body.append(node);
+  });
+
+  example(gv2, cluster, Render.XDOT).then((node) {
+    document.body.appendHtml("<h1>Cluster (xdot output)</h1>");
+    document.body.append(node);
+  });
 }
